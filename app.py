bfc3555f546d5b5213aafbd4f24c18bf7eb94ff0
Zaid <itzzaid@pm.me>
2023-07-03 21:03:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, Telethon Music Bot Is Running!'
 
-os.system("git clone $REPO_URL ok && cd ok && pip3 install -r requirements && $START_CMD")
+os.system("git clone $REPO_URL ok && cd ok && pip3 install -r requirements.txt && $START_CMD")
